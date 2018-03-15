ecfe52a01b639653e420cb6b0eb80f0e3adc63cb
Lingaraj Srinivasaperumal <Lingaraj.Srinivasaperumal@tre.se>
2018-03-15 19:13:58 +0100

    added endpoint for getting user info

--- src/main/java/se/tre/oauth/controller/UserApi.java
+++ src/main/java/se/tre/oauth/controller/UserApi.java
@@ -1,6 +1,8 @@
 package se.tre.oauth.controller;
 
 import com.google.common.collect.ImmutableMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,9 +14,12 @@ import java.security.Principal;
 @RestController
 public class UserApi {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserApi.class);
+
     @RequestMapping(value = "/user/info", method = RequestMethod.GET)
     public ResponseEntity getUserDetails(Principal user) {
         if (user != null) {
+            LOGGER.info("user "+user.getName());
             User userDetails = new User(user.getName());
             return ResponseEntity.ok(ImmutableMap.of("user", userDetails));
         }
